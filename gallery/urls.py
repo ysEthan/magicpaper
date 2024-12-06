e6085063208f9e3434d8ec7aa3f2969f43f36e3e
Ethan <757819951@qq.com>
2024-12-06 12:32:15 +0800

    增加category 删除功能

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,5 +6,6 @@ app_name = 'gallery'
 urlpatterns = [
     path('category/', views.category_list, name='category_list'),
     path('category/add/', views.category_add, name='category_add'),
-    path('category/<int:pk>/edit/', views.category_edit, name='category_edit'),
+    path('category/<int:pk>/update/', views.category_update, name='category_update'),
+    path('category/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ] 
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
 from .models import Category
 from .forms import CategoryForm
 
@@ -29,4 +30,34 @@ def category_add(request):
         'title': '添加类目',
         'submit_text': '添加'
     })
+
+@login_required
+def category_update(request, pk):
+    """修改类目页面"""
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, request.FILES, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '类目修改成功！')
+            return redirect('gallery:category_list')
+    else:
+        form = CategoryForm(instance=category)
+    
+    return render(request, 'gallery/category/form.html', {
+        'form': form,
+        'title': '修改类目',
+        'submit_text': '保存'
+    })
+
+@login_required
+def category_delete(request, pk):
+    """删除类目"""
+    category = get_object_or_404(Category, pk=pk)
+    try:
+        category.delete()
+        messages.success(request, '类目删除成功！')
+    except Exception as e:
+        messages.error(request, f'删除失败：{str(e)}')
+    return redirect('gallery:category_list')
 
